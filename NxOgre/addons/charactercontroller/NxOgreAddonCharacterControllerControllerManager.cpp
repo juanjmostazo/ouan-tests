d15976bf588fa65781456d329c781fef472cf7f9
juanj.mostazo <juanj.mostazo@6899d1ce-2719-11df-8847-f3d5e5ef3dde>
2010-03-29 17:22:48 +0000

    Added some improvements to characters & volumes

--- NxOgre/addons/charactercontroller/NxOgreAddonCharacterControllerControllerManager.cpp
+++ NxOgre/addons/charactercontroller/NxOgreAddonCharacterControllerControllerManager.cpp
@@ -103,9 +103,9 @@ Controller*  ControllerManager::createCapsuleController(const ControllerDescript
  return controller;
 }
 */
-Controller*  ControllerManager::createCapsuleController(const ControllerDescription& description, const Vec2& size, Scene* scene, PointRenderable* renderable, const std::string objectName)
+Controller*  ControllerManager::createCapsuleController(const ControllerDescription& description, const Vec2& size, Scene* scene, PointRenderable* renderable, const std::string objectName, double objectMass)
 {
-	Controller* controller = new Controller(description, size, renderable, scene, this, objectName);
+	Controller* controller = new Controller(description, size, renderable, scene, this, objectName, objectMass);
 	mControllers.insert(controller);
 	return controller;
 }
