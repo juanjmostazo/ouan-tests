4884fee1c6fea2e87c967fd8c368628f29103752
juanj.mostazo <juanj.mostazo@6899d1ce-2719-11df-8847-f3d5e5ef3dde>
2010-06-06 08:25:43 +0000

    More glow stuff refactored a bit resources folder

--- trunk/ouan-tests/TestGlow/Glow.cpp
+++ trunk/ouan-tests/TestGlow/Glow.cpp
@@ -43,13 +43,8 @@ int main(int argc, char **argv)
     try {
         app.go();
     } catch( Exception& e ) {
-#if OGRE_PLATFORM == OGRE_PLATFORM_WIN32
-        MessageBox( NULL, e.getFullDescription().c_str(), "An exception has occured!", MB_OK | MB_ICONERROR | MB_TASKMODAL);
-#else
         std::cerr << "An exception has occured: " << e.getFullDescription();
-#endif
     }
-
 
     return 0;
 }
